40427af0728df9b686cede7a3f1f96b50a983c8f
Ervin <webervin@gmail.com>
2014-09-18 23:08:37 +0300

    use class object to describe class

--- spec/lib/person_spec.rb
+++ spec/lib/person_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 require 'person'
 
-describe 'Person' do
-  subject { Person.new example.metadata[:age] }
+# describe 'Person' do
+describe Person do
+  # subject { Person.new example.metadata[:age] }
+  subject { described_class.new example.metadata[:age] }
 
   young_age = 26
-  # context "with age #{young_age}" do
   context "with age #{young_age}", age: young_age do
-    # subject { Person.new young_age }
 
     it 'should be young' do
       assert subject.young?
@@ -15,9 +15,7 @@ describe 'Person' do
   end
 
   non_young_age = 27
-  # context "with age #{non_young_age}" do
   context "with age #{non_young_age}", age: non_young_age do
-    # subject { Person.new non_young_age }
 
     it 'should not be young' do
       assert !subject.young?
