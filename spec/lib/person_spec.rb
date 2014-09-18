ade5dc852d692ef1d18f520fbbb6ba919cdac9cb
Ervin <webervin@gmail.com>
2014-09-18 23:07:21 +0300

    use metadata storage

--- spec/lib/person_spec.rb
+++ spec/lib/person_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 require 'person'
 
 describe 'Person' do
+  subject { Person.new example.metadata[:age] }
+
   young_age = 26
-  context "with age #{young_age}" do
-    subject { Person.new young_age }
+  # context "with age #{young_age}" do
+  context "with age #{young_age}", age: young_age do
+    # subject { Person.new young_age }
 
     it 'should be young' do
       assert subject.young?
@@ -12,8 +15,9 @@ describe 'Person' do
   end
 
   non_young_age = 27
-  context "with age #{non_young_age}" do
-    subject { Person.new non_young_age }
+  # context "with age #{non_young_age}" do
+  context "with age #{non_young_age}", age: non_young_age do
+    # subject { Person.new non_young_age }
 
     it 'should not be young' do
       assert !subject.young?
