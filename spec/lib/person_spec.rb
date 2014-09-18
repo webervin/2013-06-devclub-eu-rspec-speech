cffafa68e826293a57a4d8d4bc15483a04fa8999
Ervin <webervin@gmail.com>
2014-09-18 23:12:36 +0300

    use iterators to cover all ages

--- spec/lib/person_spec.rb
+++ spec/lib/person_spec.rb
@@ -1,24 +1,29 @@
 require 'spec_helper'
 require 'person'
 
-# describe 'Person' do
 describe Person do
-  # subject { Person.new example.metadata[:age] }
   subject { described_class.new example.metadata[:age] }
 
-  young_age = 26
-  context "with age #{young_age}", age: young_age do
+  # young_age = 26
+  maturity_age = 27
+  (0...maturity_age).each do |age|
+    # context "with age #{young_age}", age: young_age do
+    context "with age #{age}", age: age do
 
-    it 'should be young' do
-      assert subject.young?
+      it 'should be young' do
+        assert subject.young?
+      end
     end
   end
 
-  non_young_age = 27
-  context "with age #{non_young_age}", age: non_young_age do
+  # non_young_age = 27
+  (maturity_age..99).each do |age|
+    # context "with age #{non_young_age}", age: non_young_age do
+    context "with age #{age}", age: age do
 
-    it 'should not be young' do
-      assert !subject.young?
+      it 'should not be young' do
+        assert !subject.young?
+      end
     end
   end
 end
