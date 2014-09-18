c4e62b55e9e1e299f224ef3bfa0b47ce65b2f060
Ervin <webervin@gmail.com>
2014-09-18 23:35:45 +0300

    description of example could be read from source code of example

--- spec/getting_mature.rb
+++ spec/getting_mature.rb
@@ -7,8 +7,8 @@ shared_examples_for :getting_mature do
   (0...self.metadata[:maturity_age]).each do |age|
     context "with age #{age}", age: age do
 
-      it 'should be young' do
-        # subject.should be_young
+      # it 'should be young' do
+      it do
         should be_young
       end
     end
@@ -16,8 +16,8 @@ shared_examples_for :getting_mature do
 
   (self.metadata[:maturity_age]..self.metadata[:end_of_ages]).each do |age|
     context "with age #{age}", age: age do
-      it 'should not be young' do
-        # subject.should_not be_young
+      # it 'should not be young' do
+      it do
         should_not be_young
       end
     end
