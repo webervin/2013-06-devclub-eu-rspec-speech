042c031eeb17132d0f99e563104ebd2a553609e7
Ervin <webervin@gmail.com>
2014-09-18 23:27:26 +0300

    removed local variables from shared example group

--- spec/getting_mature.rb
+++ spec/getting_mature.rb
@@ -4,8 +4,7 @@ require 'spec_helper'
 shared_examples_for :getting_mature do
   subject { described_class.new example.metadata[:age] }
 
-  maturity_age = self.metadata[:maturity_age]
-  (0...maturity_age).each do |age|
+  (0...self.metadata[:maturity_age]).each do |age|
     context "with age #{age}", age: age do
 
       it 'should be young' do
@@ -14,8 +13,7 @@ shared_examples_for :getting_mature do
     end
   end
 
-  end_of_ages = self.metadata[:end_of_ages]
-  (maturity_age..end_of_ages).each do |age|
+  (self.metadata[:maturity_age]..self.metadata[:end_of_ages]).each do |age|
     context "with age #{age}", age: age do
       it 'should not be young' do
         assert !subject.young?
