c4764a7f620987ba3c877ff09e1c9ef97a7a544c
Ervin <webervin@gmail.com>
2014-09-18 23:03:21 +0300

    high level view, to see very similar subjects

--- spec/lib/person_spec.rb
+++ spec/lib/person_spec.rb
@@ -3,10 +3,7 @@ require 'person'
 
 describe 'Person' do
   young_age = 26
-
-  # context 'with age 26' do
   context "with age #{young_age}" do
-    # subject { Person.new 26 }
     subject { Person.new young_age }
 
     it 'should be young' do
@@ -15,10 +12,7 @@ describe 'Person' do
   end
 
   non_young_age = 27
-
-  # context "with age 27" do
   context "with age #{non_young_age}" do
-    # subject { Person.new 27 }
     subject { Person.new non_young_age }
 
     it 'should not be young' do
