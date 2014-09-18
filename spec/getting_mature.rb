645c82a261e2260cb74b57a3eb7cbfd4a02c3088
Ervin <webervin@gmail.com>
2014-09-18 23:20:18 +0300

    refactor person spec to be shared_example

---
+++ spec/getting_mature.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+# require 'person'
+
+#describe Person do
+shared_examples_for :getting_mature do
+  subject { described_class.new example.metadata[:age] }
+
+  # maturity_age = 27
+  maturity_age = self.metadata[:maturity_age]
+  (0...maturity_age).each do |age|
+    context "with age #{age}", age: age do
+
+      it 'should be young' do
+        assert subject.young?
+      end
+    end
+  end
+
+  end_of_ages = self.metadata[:end_of_ages]
+  # (maturity_age..99).each do |age|
+  (maturity_age..end_of_ages).each do |age|
+    context "with age #{age}", age: age do
+
+      it 'should not be young' do
+        assert !subject.young?
+      end
+    end
+  end
+end
