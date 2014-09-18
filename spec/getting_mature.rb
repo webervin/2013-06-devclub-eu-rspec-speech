5f7ca55dc5222df95b16ffceb3c68be46e055eff
Ervin <webervin@gmail.com>
2014-09-18 23:36:39 +0300

    use {} block syntax to boost readability

--- spec/getting_mature.rb
+++ spec/getting_mature.rb
@@ -8,18 +8,14 @@ shared_examples_for :getting_mature do
     context "with age #{age}", age: age do
 
       # it 'should be young' do
-      it do
-        should be_young
-      end
+      it { should be_young }
     end
   end
 
   (self.metadata[:maturity_age]..self.metadata[:end_of_ages]).each do |age|
     context "with age #{age}", age: age do
       # it 'should not be young' do
-      it do
-        should_not be_young
-      end
+      it { should_not be_young }
     end
   end
 end
