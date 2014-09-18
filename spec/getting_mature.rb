fb3dca021abc21d6077f44de5cd860e246d538ec
Ervin <webervin@gmail.com>
2014-09-18 23:42:19 +0300

    expectation on property using 'its'

--- spec/getting_mature.rb
+++ spec/getting_mature.rb
@@ -1,23 +1,19 @@
 require 'spec_helper'
-# require 'person'
-
 shared_examples_for :getting_mature do
   subject { described_class.new example.metadata[:age] }
-
   maturity_age = self.metadata[:maturity_age]
   (0...maturity_age).each do |age|
     context "with age #{age}", age: age do
-
-      # it 'should be young' do
       it { should be_young }
+      its(:mood) { should == :happy}
     end
   end
 
   end_of_ages = self.metadata[:end_of_ages]
   (maturity_age..end_of_ages).each do |age|
     context "with age #{age}", age: age do
-      # it 'should not be young' do
       it { should_not be_young }
+      its(:mood) { should == :average}
     end
   end
 end
