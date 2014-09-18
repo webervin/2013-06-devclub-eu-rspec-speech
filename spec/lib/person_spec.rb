c9d86fdc660a353e03066cecfc58858640800b5b
Ervin <webervin@gmail.com>
2014-09-18 23:13:36 +0300

    dropped comments to clean up spec

--- spec/lib/person_spec.rb
+++ spec/lib/person_spec.rb
@@ -4,10 +4,8 @@ require 'person'
 describe Person do
   subject { described_class.new example.metadata[:age] }
 
-  # young_age = 26
   maturity_age = 27
   (0...maturity_age).each do |age|
-    # context "with age #{young_age}", age: young_age do
     context "with age #{age}", age: age do
 
       it 'should be young' do
@@ -16,9 +14,7 @@ describe Person do
     end
   end
 
-  # non_young_age = 27
   (maturity_age..99).each do |age|
-    # context "with age #{non_young_age}", age: non_young_age do
     context "with age #{age}", age: age do
 
       it 'should not be young' do
